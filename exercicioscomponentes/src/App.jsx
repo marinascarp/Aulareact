98103ae233f58e71071eb9c529f0b6b4c0864b0c
Mari⁷ <165658502+marinascarp@users.noreply.github.com>
2025-04-22 15:01:50 -0300

    22/04/25
    aaa

--- exercicioscomponentes/src/App.jsx
+++ exercicioscomponentes/src/App.jsx
@@ -1,4 +1,6 @@
 import Exemplo1 from "./Componentes/Exemplo1"
+import Exercicio1 from "./Componentes/Exercicio1"
+import Exercicio2 from "./Componentes/Exercicio2"
 
 export default function App()
 {
@@ -7,9 +9,22 @@ export default function App()
       <h1>Exercícios de Componentes</h1>
 
       <h1>Chamada para o Exemplo1</h1>
-      <Exemplo numero1={8} numero2={2} />
-      <Exemplo numero1={2} numero2={10}/>
-      <Exemplo numero1={7} numero={20}/>
+      <Exemplo1 numero1={8} numero2={2} />
+      <Exemplo1 numero1={10} numero2={22}/>
+      <Exemplo1 numero1={7.5} numero={5.8}/>
+      <Exemplo1 numero1={-58} numero2={65}/>
+
+      <hr />
+      <h3>Chamada para o Exercício 1</h3>
+      <Exercicio1 fah={100}/>
+      <Exercicio1 fah={50}/>
+      <Exercicio1 fah={32}/>
+
+      <hr/>
+      <h3>Chamadas para o Exercício 2</h3>
+      <Exercicio2 peso="80" altura= "1.80" />
+      <Exercicio2 peso="60" altura="1.60"/>
+
 
     </div>
   )
---
+++ exercicioscomponentes/src/Componentes/Exercicio2.jsx
@@ -0,0 +1,4 @@
+export default function Exercicio2({peso, altura})
+{
+
+}
