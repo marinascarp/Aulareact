9f5380b6f2b9f4ddb7cfdddaa411eb005e7c6b95
Mari⁷ <165658502+marinascarp@users.noreply.github.com>
2025-08-05 14:51:45 -0300

    05/08/25

---
+++ estudorotas/src/Paginas/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home()
+{
+    return( 
+        <div>
+            <h1>Estudo de Rotas no React </h1>
+
+<ul>
+    <li><a href="/sp">São Paulo</a></li>
+</ul>
+
+
+        </div>
+    )
+}
---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,20 @@
+export default function SaoPaulo()
+{
+    return(
+        <div>
+            <h1>Seja bem vindo ao Estado de São Paulo</h1>
+
+            <p>O estado de São Paulo é o mais populoso do Brasil e um dos principais centros econômicos do país.<br></br>
+                 Sua capital, São Paulo, é uma metrópole global conhecida pela diversidade cultural e oportunidades de negócios.<br></br>
+                  Além da indústria e comércio, o estado possui uma rica produção agrícola e importantes áreas de turismo.
+</p>
+<p>
+    <img src="" />
+</p>
+
+<p>
+    <a href="/">Voltar para Home</a>
+</p>
+        </div>
+    )
+}
