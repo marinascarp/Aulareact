6940ba32ea1004629e410c15487bbcf654893ee6
Mari⁷ <165658502+marinascarp@users.noreply.github.com>
2025-09-02 14:54:23 -0300

    02/09/25

--- estudoestados/src/Paginas/Exemplo2.jsx
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -3,7 +3,35 @@ export default function Exemplo2() {
     <div>
       <h1>Exemplo 2</h1>
 
+      <div className="conteudo">
+        <h2>Calculo da Média Aritmética</h2>
+    
+      <p>O objeto será receber o nome do aluno, suas 3 notas e calcular a média</p>
+
+      <form>
+        <p>
+            Nome do Aluno<br/>
+          <input type="text"/>
+        </p>
+
+      <p>
+        Nota 1<br/>
+        <input type="text"/>
+      </p>
+
+      <p>
+        Nota 2<br/>
+        <input type="text"/>
+      </p>
       
+     <p>
+      <input type="button" value="calcular"/>
+     </p>
+
+      </form>
+      </div>
+
+
     </div>
   );
 }
